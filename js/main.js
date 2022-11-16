d331a02e510a40e236e2b9509bed5537f316b145
Alex100688 <alessiozingaro2@gmail.com>
2022-11-16 18:55:55 +0100

    Esegue un ciclo for per poter ciclare su 10 mail.

--- js/main.js
+++ js/main.js
@@ -10,6 +10,7 @@ const { createApp } = Vue
       }
     },
         created(){
+            //faccio un ciclo for//
            for (let i = 1 ; i <= 10; i++) {
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
             .then((response) => {
